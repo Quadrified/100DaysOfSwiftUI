d6ac690af3218b9f6dce693ae76c81df694b4b2f
Omer Quadri <omerquadri01@gmail.com>
2026-06-11 14:33:49 -0400

    Test

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,36 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		VStack {
-			LinearGradient(
-				stops: [
-					.init(color: .white, location: 0.45),
-					.init(color: .mint, location: 0.55),
-				],
-				startPoint: .top,
-				endPoint: .bottom
-			)
-
-			RadialGradient(
-				colors: [.mint, .black],
-				center: .center,
-				startRadius: 20,
-				endRadius: 200
-			)
-
-			AngularGradient(
-				colors: [
-					.indigo, .blue, .green, .yellow, .orange, .red,
-				],
-				center: .center
-			)
-
-			Text("Your content")
-				.frame(maxWidth: .infinity, maxHeight: .infinity)
-				.foregroundStyle(.white)
-				.background(.red.gradient)
-		}
-		.ignoresSafeArea()
+		Text("Hello, World!")
 	}
 }
 
