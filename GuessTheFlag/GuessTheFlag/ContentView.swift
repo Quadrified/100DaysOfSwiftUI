bc6a2a2cd93a9053a80ef3c2702a4249eeb570bd
Omer Quadri <omerquadri01@gmail.com>
2026-06-11 21:29:13 -0400

    Project 2.5 - Showing Alerts

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,59 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var showingAlert = false
+
 	var body: some View {
-		// Button
-		VStack {
-			Button("Button 1", action: executeDelete)
-				.buttonStyle(.bordered)
-
-			Button("Button 2", role: .destructive) {}
-				.buttonStyle(.bordered)
-
-			Button("Button 3") {}
-				.buttonStyle(.borderedProminent)
-				.tint(.indigo)
-
-			Button("Button 4", role: .destructive) {}
-				.buttonStyle(.borderedProminent)
-
-			Button {
-				print("Button was tapped")
-			} label: {
-				Text("Tap me!")
-					.padding()
-					.foregroundStyle(.white)
-					.background(.red)
-			}
-
-			Button("Edit", systemImage: "pencil") {
-				print("Button was tapped")
-			}
-
-			Button {
-				print("Button was tapped")
-			} label: {
-				Label("Edit", systemImage: "pencil")
-					.padding()
-					.foregroundStyle(.white)
-					.background(.red)
-			}
+		Button("Show alert") {
+			showingAlert = true
 		}
-
-		// Image
-		VStack {
-			Image(decorative: "testImage")
-
-			Image(systemName: "pencil.circle")
-				.foregroundStyle(.red)
-				.font(.largeTitle)
-
+		.alert("Important message", isPresented: $showingAlert) {
+			Button("Delete", role: .destructive) {}
+			Button("Cancel", role: .cancel) {}
+		} message: {
+			Text("Please read this!")
 		}
-
-	}
-
-	func executeDelete() {
-		print("Now deleting...")
 	}
 }
 
