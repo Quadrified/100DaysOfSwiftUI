a7600d8c2d961188ef6aac4b3d2a76377e0b8a2c
Omer Quadri <omerquadri01@gmail.com>
2026-06-11 21:18:29 -0400

    Project 2.4 - Buttons and Images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		Text("Hello, World!")
+		// Button
+		VStack {
+			Button("Button 1", action: executeDelete)
+				.buttonStyle(.bordered)
+
+			Button("Button 2", role: .destructive) {}
+				.buttonStyle(.bordered)
+
+			Button("Button 3") {}
+				.buttonStyle(.borderedProminent)
+				.tint(.indigo)
+
+			Button("Button 4", role: .destructive) {}
+				.buttonStyle(.borderedProminent)
+
+			Button {
+				print("Button was tapped")
+			} label: {
+				Text("Tap me!")
+					.padding()
+					.foregroundStyle(.white)
+					.background(.red)
+			}
+
+			Button("Edit", systemImage: "pencil") {
+				print("Button was tapped")
+			}
+
+			Button {
+				print("Button was tapped")
+			} label: {
+				Label("Edit", systemImage: "pencil")
+					.padding()
+					.foregroundStyle(.white)
+					.background(.red)
+			}
+		}
+
+		// Image
+		VStack {
+			Image(decorative: "testImage")
+
+			Image(systemName: "pencil.circle")
+				.foregroundStyle(.red)
+				.font(.largeTitle)
+
+		}
+
+	}
+
+	func executeDelete() {
+		print("Now deleting...")
 	}
 }
 
